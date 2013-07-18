2c92590b0e07b659c5fe55d6464cdd144bdeab78
Joe VLcek <jvlcek@redhat.com>
2013-07-18 10:16:50 -0400

    Add support for reading bugzilla credentials a YAML file.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,37 @@
+require 'yaml'
+
 module ApplicationHelper
+
+  # Ruby will catch and raise Erron::ENOENT: If there the user does  not
+  # have a ~/bugzilla_credentials.yaml file.
+  def get_bugzilla_creds
+    creds_file = File.expand_path('~') + '/bugzilla_credentials.yaml'
+
+    begin
+      creds = YAML.load_file(creds_file)
+    rescue Errno::ENOENT => error
+        raise "#{error.message}\n" +
+          "Please create the YAML file:#{creds_file} with valid credentials." 
+    end
+    return creds[:bugzilla_credentials][:username],
+      creds[:bugzilla_credentials][:password]
+  end
+
+  # Running "bugzilla login" generates the file ~/.bugzillacookies
+  # If that file exists assume the user already logged in.
+  def logged_in?
+    File.exists?(File.expand_path('~') + '/.bugzillacookies')
+  end
+
+  def login!
+    raise "Please install python-bugzilla" unless
+      File.exists?(File.expand_path('/usr/bin/bugzilla'))
+
+    username, password = self.get_bugzilla_creds
+    login_cmd = "/usr/bin/bugzilla login " \
+                "\'#{username}\' " \
+                "\'#{password}\' "
+    %x[ #{login_cmd} ]         
+  end                          
+
 end
--- app/models/query.rb
+++ app/models/query.rb
@@ -1,15 +1,15 @@
+include ApplicationHelper
+
 class Query < ActiveRecord::Base
   attr_accessible :bug_status, :description, :flag, :name, :output_format, :product
 
-  def run( query )
+  def run(query)
+    BugzillaAccess.login! unless BugzillaAccess.logged_in?
     query_cmd = "/usr/bin/bugzilla query " \
                 "--product=\"#{query.product}\" " \
                 "--flag=#{query.flag} " \
                 "--bug_status=#{query.bug_status} " \
                 "--outputformat=\'#{query.output_format}\'"
-
-    # %x[/usr/bin/bugzilla query --product=\"CloudForms Management Engine\" --flag= --bug_status=POST --outputformat='%{id},%{flags}']
-
     %x[ #{query_cmd} ]
   end
 end
