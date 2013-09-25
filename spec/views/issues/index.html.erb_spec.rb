3a24af0d063c1d1fb6a2e40771db5ff98080239d
Joe VLcek <jvlcek@redhat.com>
2013-09-25 14:57:43 -0400

    Add a views/issues spec

---
+++ spec/views/issues/index.html.erb_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe "issues/index" do
+  before(:each) do
+    assign(:issues, [
+      stub_model(Issue,
+        :bz_id => "Bz",
+        :status => "Status",
+        :summary => "Summary",
+        :assigned_to => "Assignee",
+        :version => "Version",
+        :version_ack => "Version Ack",
+        :pm_ack => "Pm Ack",
+        :devel_ack => "Devel Ack",
+        :qa_ack => "Qa Ack",
+        :doc_ack => "Doc Ack"
+      ),
+      stub_model(Issue,
+        :bz_id => "Bz",
+        :status => "Status",
+        :summary => "Summary",
+        :assigned_to => "Assignee",
+        :version => "Version",
+        :version_ack => "Version Ack",
+        :pm_ack => "Pm Ack",
+        :devel_ack => "Devel Ack",
+        :qa_ack => "Qa Ack",
+        :doc_ack => "Doc Ack"
+      )
+    ])
+  end
+
+  context "#index" do
+    it "renders a list of issues" do
+      render
+      assert_select "tr>td", :text => "Bz".to_s, :count => 2
+      assert_select "tr>td", :text => "Status".to_s, :count => 2
+      assert_select "tr>td", :text => "Assignee".to_s, :count => 2
+      assert_select "tr>td", :text => "Summary".to_s, :count => 2
+    end
+  end
+end
