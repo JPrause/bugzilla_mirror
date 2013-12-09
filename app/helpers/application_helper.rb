7d6a78025028cad1ecd85099f40cd243906172fb
Joe VLcek <jvlcek@redhat.com>
2013-12-09 18:51:08 -0500

    Streamline setting up the bug display uri

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
 
-  def get_bugzilla_uri
-    AppConfig['bugzilla']['uri'] + "/show_bug.cgi?id="
-  end
-
   def sortable(column, title = nil)
     title ||= column.titleize
     direction = column == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -6,3 +6,5 @@ AppConfig = YAML.load_file(Rails.root.join('config', 'cfme_bz.yml'))[Rails.env] 
 AppConfig['bugzilla']['uri'] ||= "https://bugzilla.redhat.com/"
 AppConfig['bugzilla']['product'] ||= "CloudForms Management Engine"
 
+AppConfig['bugzilla']['bug_display_uri'] = AppConfig['bugzilla']['uri'] + "/show_bug.cgi?id="
+
