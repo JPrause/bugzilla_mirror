24d229b53ff8f7706d534b2ee4eaf47c827e1102
Joe VLcek <jvlcek@redhat.com>
2013-12-09 18:29:33 -0500

    Correct wrong default bugzilla URI and use cleaner syntax.

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -3,8 +3,6 @@ require 'yaml'
 
 AppConfig = YAML.load_file(Rails.root.join('config', 'cfme_bz.yml'))[Rails.env] rescue {"bugzilla"=>{}}
 
-AppConfig['bugzilla']['uri'] = "https://bugzilla.redhat.com/show_bug.cgi?id=" unless AppConfig['bugzilla']['uri']
-AppConfig['bugzilla']['product'] = "CloudForms Management Engine" unless AppConfig['bugzilla']['product']
-
-
+AppConfig['bugzilla']['uri'] ||= "https://bugzilla.redhat.com/"
+AppConfig['bugzilla']['product'] ||= "CloudForms Management Engine"
 
