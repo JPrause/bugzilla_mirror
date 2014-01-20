374b8850975a289dd9135ae1c85b417e4437ef62
Joe VLcek <jvlcek@redhat.com>
2014-01-20 14:27:51 -0500

    Add skeleton for version selection.

--- app/controllers/errata_reports_controller.rb
+++ app/controllers/errata_reports_controller.rb
@@ -4,31 +4,31 @@ class ErrataReportsController < ApplicationController
   # GET /errata_reports.json
   def index
 
+    puts "JJV -010- index params ->#{params}<-"
+    puts "JJV -011- index params[\"version\"] ->#{params["version"]}<-"
+
     @need_acks = []
     @have_acks = []
 
     # TODO: When where.not method becomes available in Rails 4 this
     #       logic could possibly be simplified.
-    Issue.where(:status => "POST").order(sort_column + " " + sort_direction).
-      order(:version).each do |bz|
+    Issue.where(:status => "POST").order(sort_column + " " + sort_direction).order(:version).each do |bz|
+        entry = BugEntry.new(
+          :BZ_ID      => bz.bz_id,
+          :DEP_ID     => bz.dep_id,
+          :PM_ACKS    => bz.pm_ack == "+" ? "X" : " ",
+          :DEVEL_ACKS => bz.devel_ack == "+" ? "X" : " ",
+          :QA_ACKS    => bz.qa_ack == "+" ? "X" : " ",
+          :DOC_ACKS   => bz.doc_ack == "+" ? "X" : " ",
+          :VER_ACKS   => bz.version_ack == "+" ? "X" : " ",
+          :VERSION    => bz.version,
+          :SUMMARY    => bz.summary)
 
-      entry = BugEntry.new(
-        :BZ_ID      => bz.bz_id,
-        :DEP_ID     => bz.dep_id,
-        :PM_ACKS    => bz.pm_ack == "+" ? "X" : " ",
-        :DEVEL_ACKS => bz.devel_ack == "+" ? "X" : " ",
-        :QA_ACKS    => bz.qa_ack == "+" ? "X" : " ",
-        :DOC_ACKS   => bz.doc_ack == "+" ? "X" : " ",
-        :VER_ACKS   => bz.version_ack == "+" ? "X" : " ",
-        :VERSION    => bz.version,
-        :SUMMARY    => bz.summary)
-
-      if has_all_acks?(entry)
-        @have_acks << entry
-      else
-        @need_acks << entry
-      end
-
+        if has_all_acks?(entry)
+          @have_acks << entry
+        else
+          @need_acks << entry
+        end
     end
 
     respond_to do |format|
@@ -42,6 +42,16 @@ class ErrataReportsController < ApplicationController
   def has_all_acks?(entry)
       a = [entry[:PM_ACKS], entry[:DEVEL_ACKS], entry[:QA_ACKS], entry[:DOC_ACKS], entry[:VER_ACKS]]
       (a & a).size == 1
+  end
+
+  def display_version?(bz, requested_version)
+    puts "JJV -070- display_version? requested_version ->#{requested_version}<-"
+    
+    if requested_version == "All" || requested_version == bz.version
+      true
+    else
+      false
+    end
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def sortable(column, title = nil)
     title ||= column.titleize
     direction = column == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
-    link_to title, :sort => column, :direction => direction
+    link_to title, :sort => column, :direction => direction, :version => params[:version]
   end
 
   def url_to_bugzilla(bz_id)
@@ -15,6 +15,18 @@ module ApplicationHelper
     bz_ids.collect { |bz_id|
       url_to_bugzilla(bz_id)
     }.join(" ").html_safe
+  end
+
+  def list_last_commit(bz)
+    puts "JJV -090- list_last_commit(#{bz.bz_id}, #{bz.dep_ids})"
+    "5.2.0.37" # JJV Hard code for initial testing
+  end
+     
+  def available_versions()
+    result = Issue.uniq.pluck(:version) # JJV
+     # JJV ["cfme-5.2", "cfme-5.1.z", "cfme-5.3", "cfme-4.0.z", "NONE", "cfme-5.0.z", "cfme-5.2.z"] 
+    puts "JJV -070- available_versions() result ->#{result}<-"
+    Issue.uniq.pluck(:version)
   end
      
 end
