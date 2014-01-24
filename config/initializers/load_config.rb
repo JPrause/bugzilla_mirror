1b7618aa2d5c04da8396c0f6ad2525bb65ff2bee
Joe Rafaniello <jrafanie@redhat.com>
2014-01-24 16:15:37 -0500

    Don’t load the cfme_bz.yml in the test environment.

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,10 +1,11 @@
 require 'ostruct'
 require 'yaml'
 
-AppConfig = YAML.load_file(Rails.root.join('config', 'cfme_bz.yml'))[Rails.env] rescue {"bugzilla"=>{}}
+unless Rails.env.test?
+  AppConfig = YAML.load_file(Rails.root.join('config', 'cfme_bz.yml'))[Rails.env] rescue {"bugzilla"=>{}}
+  AppConfig['bugzilla']['uri'] ||= "https://bugzilla.redhat.com/"
+  AppConfig['bugzilla']['product'] ||= "CloudForms Management Engine"
 
-AppConfig['bugzilla']['uri'] ||= "https://bugzilla.redhat.com/"
-AppConfig['bugzilla']['product'] ||= "CloudForms Management Engine"
-
-AppConfig['bugzilla']['bug_display_uri'] = AppConfig['bugzilla']['uri'] + "/show_bug.cgi?id="
+  AppConfig['bugzilla']['bug_display_uri'] = AppConfig['bugzilla']['uri'] + "/show_bug.cgi?id="
+end
 
