f244d6f4a6b15fe7461e717f30e283452e7b74dd
Joe Rafaniello <jrafanie@redhat.com>
2014-01-24 16:17:01 -0500

    Added Commit specs and did some refactorings.

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -4,26 +4,30 @@ class Commit < ActiveRecord::Base
   attr_accessible :branch, :sha_id
 
   def self.update_from_git!
-    raise ArgumentError, "cfme git checkout not defined in config/cfme_bz.yml" unless AppConfig["cfme_git"]["repo_path"]
-    raise ArgumentError,  "Error no password specified in config/cfme_bz.yml" unless AppConfig["bugzilla"]["password"]
-
+    raise ArgumentError, "cfme git checkout not defined in config/cfme_bz.yml" unless cfme_git_configuration["repo_path"]
+    raise ArgumentError, "cfme git releases not defined in config/cfme_bz.yml" unless cfme_git_configuration["releases"]
     self.destroy_all
 
-    branches  = AppConfig["cfme_git"]["releases"].collect {|_, branches| branches}.flatten.uniq
-
-    repo_path = AppConfig["cfme_git"]["repo_path"]
+    branches  = cfme_git_configuration["releases"].collect {|_, branches| branches}.flatten.uniq
     branches.each do |branch|
-      CFMEGit::Processor.new(repo_path, branch).commits_referencing_bz_urls.each do |commit|
-        issues = Issue.where(:bz_id => commit.bz_ids).all
-        # attributes = commit.attributes.merge(:issues => issues)
-        c = self.new(commit.attributes)
-        c.issues = issues
-        c.save!
-      end
+      update_git_branch(branch)
     end
   end
 
-  def inspect
-    "branch: #{branch}, sha_id: #{sha_id}\n"
+  def self.update_git_branch(branch)
+    processor = CFMEGit::Processor.new(repo_path, branch)
+    processor.commits_referencing_bz_urls.each do |commit|
+      c = Commit.new(commit.attributes)
+      c.issues = Issue.where(:bz_id => commit.bz_ids).all
+      c.save!
+    end
+  end
+
+  def self.repo_path
+    cfme_git_configuration["repo_path"]
+  end
+
+  def self.cfme_git_configuration
+    AppConfig["cfme_git"]
   end
 end
--- spec/models/commit_spec.rb
+++ spec/models/commit_spec.rb
@@ -1,5 +1,78 @@
 require 'spec_helper'
 
 describe Commit do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context ".update_from_git!" do
+    before do
+      @config = {
+        "repo_path" => "/some_path",
+        "releases"  => {
+          "cfme-5.2.z" => ["5_2_release"]
+        }
+      }
+      described_class.stub(:cfme_git_configuration => @config)
+
+      Grit::Repo.stub(:new)
+    end
+
+    context "two branches" do
+      before do
+        @config = {
+          "repo_path" => "/some_path",
+          "releases"  => {
+            "cfme-5.2.z" => ["5_2_release"],
+            "cfme-5.3"   => ["5_3_release"]
+          }
+        }
+        described_class.stub(:cfme_git_configuration => @config)
+      end
+
+      it "normal case" do
+        double1 = double(:attributes => {}, :bz_ids => [])
+
+        CFMEGit::Processor.any_instance.stub(:commits_referencing_bz_urls => [double1])
+
+        expect { described_class.update_from_git! }.to change{ Commit.count }.by(2)
+      end
+
+      #TODO: Should we really create two commits with the same sha1?
+      it "the same commit sha1 is on two branches" do
+        double1 = double(:attributes => {:branch => "one", :sha_id => "123"}, :bz_ids => [])
+        double2 = double(:attributes => {:branch => "two", :sha_id => "123"}, :bz_ids => [])
+
+        CFMEGit::Processor.any_instance.stub(:commits_referencing_bz_urls => [double1])
+
+        expect { described_class.update_from_git! }.to change{ Commit.count }.by(2)
+      end
+    end
+
+    it "two commits referencing one issue" do
+      issue1   = Issue.create(:bz_id => 1000)
+      double1 = double(:attributes => {}, :bz_ids => [1000])
+      double2 = double(:attributes => {}, :bz_ids => [1000])
+
+      CFMEGit::Processor.any_instance.stub(:commits_referencing_bz_urls => [double1, double2])
+      described_class.update_from_git!
+
+      described_class.count.should == 2
+
+      commits = described_class.all
+      commits[0].issues.should == [issue1]
+      commits[1].issues.should == [issue1]
+    end
+
+    it "one commit referencing multiple issues" do
+      issue1   = Issue.create(:bz_id => 1000)
+      issue2   = Issue.create(:bz_id => 2000)
+      double1  = double(:attributes => {}, :bz_ids => [1000, 2000])
+
+      CFMEGit::Processor.any_instance.stub(:commits_referencing_bz_urls => [double1])
+      described_class.update_from_git!
+
+      described_class.count.should == 1
+      commit = described_class.first
+
+      issue1.commits.should == [commit]
+      issue2.commits.should == [commit]
+    end
+  end
 end
