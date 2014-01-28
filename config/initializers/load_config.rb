8f901d225e3b43d8729bf28d93c48f70bd4c7b5b
Joe Rafaniello <jrafanie@redhat.com>
2014-01-28 17:53:43 -0500

    Put back AppConfig defaults to fix some IssuesController specs.

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,11 +1,10 @@
 require 'ostruct'
 require 'yaml'
 
-unless Rails.env.test?
-  AppConfig = YAML.load_file(Rails.root.join('config', 'cfme_bz.yml'))[Rails.env] rescue {"bugzilla"=>{}}
-  AppConfig['bugzilla']['uri'] ||= "https://bugzilla.redhat.com/"
-  AppConfig['bugzilla']['product'] ||= "CloudForms Management Engine"
+AppConfig = YAML.load_file(Rails.root.join('config', 'cfme_bz.yml'))[Rails.env] rescue {"bugzilla"=>{}}
+AppConfig['bugzilla']['uri'] ||= "https://bugzilla.redhat.com/"
+AppConfig['bugzilla']['product'] ||= "CloudForms Management Engine"
 
-  AppConfig['bugzilla']['bug_display_uri'] = AppConfig['bugzilla']['uri'] + "/show_bug.cgi?id="
-end
+AppConfig['bugzilla']['bug_display_uri'] = AppConfig['bugzilla']['uri'] + "/show_bug.cgi?id="
+
 
