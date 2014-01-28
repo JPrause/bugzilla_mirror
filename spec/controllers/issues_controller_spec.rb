c168a019f52939928da248ea573799ee1d81043c
Joe Rafaniello <jrafanie@redhat.com>
2014-01-28 17:54:13 -0500

    Don’t actually process commits in the controller spec.

--- spec/controllers/issues_controller_spec.rb
+++ spec/controllers/issues_controller_spec.rb
@@ -12,6 +12,7 @@ describe IssuesController do
   describe "GET 'update_all'" do
     it "when #update_all returns http success" do
       Issue.stub(:update_from_bz).and_return("stub")
+      Commit.stub(:update_from_git!)
       get 'update_all'
       response.should be_success
     end
